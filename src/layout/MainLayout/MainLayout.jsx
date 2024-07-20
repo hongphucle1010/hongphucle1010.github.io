71edb18cff44cfd7862c98c8ca7d21db915c02a7
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-07-20 09:45:11 +0700

    feat: Add transition effect when toggling dark and light mode

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -4,12 +4,15 @@ import Footer from "../../components/Footer/Footer";
 import PropTypes from "prop-types";
 import { useContext } from "react";
 import { ThemeContext } from "../../config/ThemeConfig/ThemeConfig";
+import styles from "./MainLayout.module.scss";
 
 const MainLayout = ({ children }) => {
   const { darkMode } = useContext(ThemeContext);
   return (
     <div className={`${darkMode ? "dark" : "light"}`}>
-      <div className="dark:bg-slate-800">
+      <div
+        className={`dark:bg-slate-800 dark:text-slate-100 ${styles["transition-bg-text"]} min-h-screen`}
+      >
         <Header />
         {children}
         <Outlet />
