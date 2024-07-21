a66feddd005ecc323901828da5bc5a39432bf14c
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-07-21 11:28:10 +0700

    feat: change light icon button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { useContext } from "react";
 import ReactSwitch from "react-switch";
 import { ThemeContext } from "../../config/ThemeConfig/ThemeConfig";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const navItemArray = [
   { name: "Home", path: "/#" },
@@ -48,6 +48,26 @@ const ThreeSlashButton = () => (
   </svg>
 );
 
+const MySun = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="w-5 h-5 text-white group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-300"
+    role="img"
+    aria-labelledby="ag7g3610m2j2vg5zwj4zvvh3zomoto9y"
+  >
+    <title id="ag7g3610m2j2vg5zwj4zvvh3zomoto9y">theme icon</title>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+    />
+  </svg>
+);
+
 const ThemeSwitchButton = ({ darkMode, toggleDarkMode }) => {
   const MoonIcon = (
     <div
@@ -66,7 +86,7 @@ const ThemeSwitchButton = ({ darkMode, toggleDarkMode }) => {
         height: "100%",
       }}
     >
-      <FontAwesomeIcon icon={faSun} className="text-white" />
+      <MySun className="text-blue-400" />
     </div>
   );
   return (
